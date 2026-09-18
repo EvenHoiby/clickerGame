5875e1650b2295475c6607e0926815c3c2f9dca3
Even <evenhoiby02@gmail.com>
2026-09-18 21:25:43 +0200

    Made it say if you have bought an upgrade or not.

--- javascript.js
+++ javascript.js
@@ -14,8 +14,15 @@ const upg2 = document.getElementById('upg2');
 const upg3 = document.getElementById('upg3');
 const upg4 = document.getElementById('upg4');
 
-if(dollarAmount >= 50) {
-}
+setInterval(() => {
+    if(hasUpg1 === true) {
+        upg1.innerText = 'Sold | Cost: 50'
+    }
+    else if(hasUpg1 === false && dollarAmount >= 50) {
+        upg1.innerText = 'Buy | Cost: 50'
+    }
+}, 100)
+
 upg1.addEventListener('click', () => {
     if(dollarAmount >= 50 && hasUpg1 === false) {
     hasUpg1 = true;
