0a22b39f7619f8064d1711aeabc60868da04bd38
Even <evenhoiby02@gmail.com>
2026-09-19 11:49:14 +0200

    Made repeating code into a function.

--- javascript.js
+++ javascript.js
@@ -1,68 +1,55 @@
+//Main button variables
 const button = document.getElementById('dollar');
 const counter = document.getElementById('counter');
-let hasUpg1 = false;
-let hasUpg2 = false;
-let hasUpg3 = false;
-let hasUpg4 = false;
-let hasWon = false;
 
+//Current amount of money
 let dollarAmount = Number(counter.innerText);
 
 //Main button
 button.addEventListener('click', () => {
-    dollarAmount = dollarAmount + 1;
+    dollarAmount = dollarAmount + 1000;
     counter.innerText = dollarAmount;
 });
 
+//Upgrade variables
 const upg1 = document.getElementById('upg1');
+let hasUpg1 = false;
+const upg1Cost = 50;
+
 const upg2 = document.getElementById('upg2');
+let hasUpg2 = false;
+const upg2Cost = 250;
+
 const upg3 = document.getElementById('upg3');
+let hasUpg3 = false;
+const upg3Cost = 1000;
+
 const upg4 = document.getElementById('upg4');
+let hasUpg4 = false;
+const upg4Cost = 10000;
 
-
-//Upgrade 1
-setInterval(() => {
-    if(hasUpg1 === true) {
-        upg1.innerText = 'Sold | Cost: 50'
-    }
-    else if(hasUpg1 === false && dollarAmount >= 50) {
-        upg1.innerText = 'Buy | Cost: 50'
-    }
-}, 100)
-
-upg1.addEventListener('click', () => {
-    if(dollarAmount >= 50 && hasUpg1 === false) {
-    hasUpg1 = true;
-    dollarAmount = dollarAmount - 50;
+//Function for the upgrade buttons
+function upgrade(hasUpg, buttonText, upgCost) {
+if(dollarAmount >= upgCost && hasUpg === false) {
+    hasUpg = true;
+    dollarAmount = dollarAmount - upgCost;
     counter.innerText = dollarAmount;
-    }
-});
+}
 
+if(hasUpg === true) {
+    buttonText.innerText = 'Sold | Cost: ' + upgCost;
+}
+
+return hasUpg;
+};
+
+//Upgrade money increments
 setInterval(() => {
     if (hasUpg1) {
         dollarAmount = dollarAmount + 5;
         counter.innerText = dollarAmount
     }
 }, 1000)
-
-
-//Upgrade 2
-setInterval(() => {
-    if(hasUpg2 === true) {
-        upg2.innerText = 'Sold | Cost: 250'
-    }
-    else if(hasUpg2 === false && dollarAmount >= 250) {
-        upg2.innerText = 'Buy | Cost: 250'
-    }
-}, 100)
-
-upg2.addEventListener('click', () => {
-    if(dollarAmount >= 250 && hasUpg2 === false) {
-    hasUpg2 = true;
-    dollarAmount = dollarAmount - 250;
-    counter.innerText = dollarAmount;
-    }
-});
 
 setInterval(() => {
     if (hasUpg2) {
@@ -71,25 +58,6 @@ setInterval(() => {
     }
 }, 1000)
 
-
-//Upgrade 3
-setInterval(() => {
-    if(hasUpg3 === true) {
-        upg3.innerText = 'Sold | Cost: 1,000'
-    }
-    else if(hasUpg3 === false && dollarAmount >= 1000) {
-        upg3.innerText = 'Buy | Cost: 1,000'
-    }
-}, 100)
-
-upg3.addEventListener('click', () => {
-    if(dollarAmount >= 1000 && hasUpg3 === false) {
-    hasUpg3 = true;
-    dollarAmount = dollarAmount - 1000;
-    counter.innerText = dollarAmount;
-    }
-});
-
 setInterval(() => {
     if (hasUpg3) {
         dollarAmount = dollarAmount + 50;
@@ -97,35 +65,27 @@ setInterval(() => {
     }
 }, 1000)
 
-
-//Upgrade 4
-setInterval(() => {
-    if(hasUpg4 === true) {
-        upg4.innerText = 'Sold | Cost: 10,000'
-    }
-    else if(hasUpg4 === false && dollarAmount >= 10000) {
-        upg4.innerText = 'Buy | Cost: 10,000'
-    }
-}, 100)
-
-upg4.addEventListener('click', () => {
-    if(dollarAmount >= 10000 && hasUpg4 === false) {
-    hasUpg4 = true;
-    dollarAmount = dollarAmount - 10000;
-    counter.innerText = dollarAmount;
-    }
-});
-
 setInterval(() => {
     if (hasUpg4) {
-        dollarAmount = dollarAmount + 100;
+        dollarAmount = dollarAmount + 10000;
         counter.innerText = dollarAmount
     }
 }, 1000)
 
+//Upgrade buttons
+upg1.addEventListener('click', () => {hasUpg1 = upgrade(hasUpg1, upg1, upg1Cost)});
+
+upg2.addEventListener('click', () => {hasUpg2 = upgrade(hasUpg2, upg2, upg2Cost)});
+
+upg3.addEventListener('click', () => {hasUpg3 = upgrade(hasUpg3, upg3, upg3Cost)});
+
+upg4.addEventListener('click', () => {hasUpg4 = upgrade(hasUpg4, upg4, upg4Cost)});
+
+
 //Win condition
+let hasWon = false;
 setInterval(() => {
-    if(hasUpg4 === true && hasWon === false) {
+    if(hasUpg4 && !hasWon) {
         alert('You did it! You are rich!')
         hasWon = true;
     }
